769014d109dae878b8204550497cd5cb26308e7b
Mr ErroR <89709632+deborgo@users.noreply.github.com>
2021-09-13 20:15:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var quantidadeDeAlunos = 1
+;
+if (quantidadeDeAlunos == 0) {
+    console.log("O Número é 0");
+}else {
+    if ( quantidadeDeAlunos %2 == 0)
+    console.log("O Número " + quantidadeDeAlunos + " é par");
+else
+if(quantidadeDeAlunos % 2 == 1)
+    console.log("O Número " + quantidadeDeAlunos + " é ímpar")
+}
